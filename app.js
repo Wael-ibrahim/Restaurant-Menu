ad2cb6ef678b34dc450ae67195f144d5b4f46f03
Wael Ibrahim <wael@d753dd7f-3ec3-45af-b328-1e0c9ea9e947.fritz.box>
2020-10-28 21:23:01 +0100

    28-10-2020 finish

---
+++ app.js
@@ -0,0 +1,117 @@
+const express = require('express')
+// const dateFormat = require('dateformat')
+const sendData = require('./modules/mysqlDataModal')
+const session = require('express-session')
+const cookie = require('cookie-parser')
+
+
+const app = express()
+
+const sessionOptions = {
+    secret: 'high-class',
+    cookie: {}
+}
+
+app.use(session(sessionOptions))
+app.use(cookie())
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+app.set('views',__dirname + '/views');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/index1', (req, res) => {
+    res.render('index1');
+});
+
+app.get('/admin', (req, res) => {
+    if (req.session.user){
+        console.log(req.session.user);
+        res.redirect('/admin')
+    } else {
+        res.render('admin')
+    }
+
+});
+
+app.get('/dashboard', (req, res) => {
+    sendData.getData().then((result) => {
+        res.render('dashboard', {result});
+        console.log(result);
+    }).catch(error => {
+        res.send('404');
+    })
+    
+});
+ app.post('/submit', (req, res) => {
+     console.log(req.body);
+     const tablenumber = req.body.tablenumber
+     const fname = req.body.fname
+     const lname = req.body.lname
+     const email = req.body.email.trim()
+     const telephone = req.body.telephone.trim()
+     const address = req.body.address
+     const city = req.body.city
+     const zip = req.body.zip
+     //const now = (new Date()).toJSON().replace(/\..*$/g,'')
+     const now = new Date()
+
+     
+     //console.log(now);
+
+
+     
+     if(tablenumber && fname && lname && email || telephone && address && city && zip && now) {
+        sendData.dataSender(tablenumber, fname, lname, email, telephone, address, city, zip, now ).then(()=>{
+          
+                res.json(1);
+            
+        }).catch(error =>{
+            console.log(error);
+            if (error == "exist") {
+                res.json(3)
+            } else {
+                res.json(4)
+            }
+
+        })
+         
+     } else {
+         res.json(2);
+     }
+ });
+
+ app.post('/admin', (req, res) => {
+    console.log(req.body);
+    const username = req.body.username
+    const password = req.body.password
+
+    if(username && password ) {
+       sendData.checkUser(username, password ).then((user) => {
+        req.session.user = user
+        res.json(1);
+           
+       }).catch(error =>{
+           console.log(error);
+           if (error == 3) {
+               res.json(3)
+           } else {
+               res.json(4)
+           }
+       })       
+    } else {
+        res.json(2);
+    }
+});
+
+
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+});
---
+++ modules/mysqlDataModal.js
@@ -0,0 +1,108 @@
+const mySql = require('mysql')
+
+let con = null
+function connect() {
+    return new Promise((resolve, reject) => {
+        if (con) {
+            if (con.state === 'disconnected') {
+                con.connect(error => {
+                    if (error) {
+                        reject(error)
+                    } else {
+                        resolve()
+                    }
+                })
+            } else {
+                resolve()
+            }
+        } else {
+            con = mySql.createConnection({
+                multipleStatements: true,
+                host: 'localhost',
+                port: 3306,
+                user: 'root',
+                password: '123456789',
+                database: 'menu_db'
+            })
+            con.connect(error => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve()
+                }
+            })
+        }
+    })
+}
+
+function runQuery(queryString) {
+    return new Promise((resolve, reject) => {
+        connect().then(() => {
+            con.query(queryString, (error, result, fields) => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(result)
+                }
+            })
+        }).catch(error => {
+            reject(error)
+        })
+    })
+}
+
+function dataSender(table_number, fname, lname, email, telephone, address, city, zip, date) {
+    return new Promise((resolve, reject) => {
+        runQuery(`INSERT INTO users (table_number, fname, lname, email, telephone, address, city, zip, date) VALUES 
+        ('${table_number}', '${fname}', '${lname}', '${email}', '${telephone}', '${address}', '${city}', '${zip}', '${date}')`).then(() => {
+            resolve()
+        }).catch(error => {    
+            if (error.errno === 1062) {
+                reject(4)
+            } else {
+                reject(2)
+            }
+            
+        })
+    })
+}
+
+function checkUser(user_name, password) {
+    return new Promise((resolve, reject) => {
+        runQuery(`SELECT * FROM admin where user_name like '${user_name}'`).then((result) => {
+            if(result === 0) {
+                reject(4)
+            } else {
+                if(password === result[0].password) {
+                    resolve(result[0])
+                } else {
+                    reject(3)
+                }
+            }
+            
+        }).catch(error => {    
+            reject(error)    
+        })
+    })
+}
+
+function getData() {
+    return new Promise((resolve, reject) => {
+        runQuery(`SELECT * FROM users`).then((result) => {
+            resolve(result)
+        }).catch(error => {    
+            if (error.errno === 1062) {
+                reject(4)
+            } else {
+                reject(2)
+            }
+            
+        })
+    })
+}
+
+module.exports = {  
+    dataSender,
+    checkUser,
+    getData
+}
